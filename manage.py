9900c22dd15bfb3b387fcaf1ed38d9d57b4db9ed
mooon33 <164473198+mooon33@users.noreply.github.com>
2025-03-28 21:07:23 +1000

    begin

--- manage.py
+++ manage.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python
 import os
 import sys
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pokemon_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BankPortal.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
